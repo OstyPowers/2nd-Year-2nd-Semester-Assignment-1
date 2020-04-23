ee967482797c628f64436085f91a73a692783b63
Matt Osterfield <60369843+OstyPowers@users.noreply.github.com>
2020-04-23 15:12:16 +1200

    Add files via upload

---
+++ Assignment 1/src/sokoban/Empty.java
@@ -0,0 +1,10 @@
+package sokoban;
+
+public class Empty extends Placeable {
+	public Empty(int x,  int y) {
+		this.symbol = ".";
+		this.x = x;
+		this.y = y;
+	}
+
+}
---
+++ Assignment 1/src/sokoban/Placeable.java
@@ -0,0 +1,12 @@
+package sokoban;
+
+public abstract class Placeable {
+	protected String symbol;
+	protected int x;
+	protected int y;
+	public String toString() {
+		return this.symbol;
+		
+	}
+
+}
